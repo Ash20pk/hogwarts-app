9371dc86a7f22ba9f1716fb5566a3d97bcaddb1d
Ash20pk <ash20pk@gmail.com>
2023-09-07 12:28:01 +0530

    Major fix - everything working fine

--- src/App.js
+++ src/App.js
@@ -1,90 +1,61 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import Web3 from "web3";
-import HogwartsNFT from "./HogwartsNFT.json";
-import HogwartsLogo from "./hogwarts_logo.png";
+import HogwartsNFT from "./artifacts/HogwartsNFT.json"; 
+import RandomHouseAssignment from "./artifacts/RandomHouseAssignment.json"; 
+import HogwartsLogo from "./hogwarts_logo.png"; // import the image
 import "./App.css";
-import thinkingSound from "./thinking.mp3";
-import gryffindorSound from "./gryffindor.mp3"; 
-import slytherinSound from "./slytherin.mp3";
-import ravenclawSound from "./ravenclaw.mp3";
-import hufflepuffSound from "./hufflepuff.mp3";
+import thinkingSound from "./sounds/thinking.mp3"; // Replace "thinking.mp3" with your sound file
+
 
 const web3 = new Web3(window.ethereum);
 
 function App() {
   const [account, setAccount] = useState("");
-  const [contract, setContract] = useState(null);
+  const [hogwartsContract, setHogwartsContract] = useState(null);
+  const [randomHouseContract, setRandomHouseContract] = useState(null);  
   const [house, setHouse] = useState("");
   const [house_slogan, sethouseSlogan] = useState("");
   const [minted, setMinted] = useState(false);
   const [connected, setConnected] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [dataLoaded, setDataLoaded] = useState(false); 
+  const [loading, setLoading] = useState(false); // initialize loading state to true
+  const [checkMintedSuccess, setCheckMintSuccess] = useState(0);
 
-//Audio Files
   const thinkingAudio = new Audio(thinkingSound);
-  const gryffindorAudio = new Audio(gryffindorSound); 
-  const slytherinAudio = new Audio(slytherinSound); 
-  const ravenclawAudio = new Audio(ravenclawSound); 
-  const hufflepuffAudio = new Audio(hufflepuffSound);
-
-  const houseAudioMap = {
-    0: thinkingAudio,
-    1: gryffindorAudio,
-    2: hufflepuffAudio,
-    3: ravenclawAudio,
-    4: slytherinAudio,
-  };
-  
-
-  const getHouse = useCallback(async () => {
-    setLoading(true);
-    const addressToHouse = ["You might belong in Gryffindor....", "You might belong in Hufflepuff....", "You might belong in wise old Ravenclaw....", "You belong perhaps in Slytherin...."];
-    const houseIndex = await contract.methods.s_addressToHouse(account).call();
-    setHouse(addressToHouse[houseIndex]);
-    setLoading(false);  
-  }, [contract, account]);
-
-  const getHouseSlogan = useCallback(async () => {
-    setLoading(true);
-    const sloganToHouse = ["Where dwell the brave at heart. Their daring, nerve, and chivalry, Set Gryffindors apart.",    "Where they are just and loyal. Those patient Hufflepuffs are true And unafraid of toil.",    "If you’ve a ready mind. Where those of wit and learning, Will always find their kind.",    "You’ll make your real friends. Those cunning folks use any means, To achieve their ends."  ];      
-    const houseSloganIndex = await contract.methods.s_addressToHouse(account).call();
-    sethouseSlogan(sloganToHouse[houseSloganIndex]);
-    setLoading(false);
-  }, [contract, account]);
-
-  const checkMinted = useCallback(async () => {
-    const minted = await contract.methods.balanceOf(account).call();
-    if (minted > 0){
-      setMinted(true);
-      getHouse();
-      getHouseSlogan();
-    }
-    else
-    setMinted(false);
-  }, [contract, account, getHouse, getHouseSlogan]);
 
   useEffect(() => {
     if (window.ethereum) {
+      // set the initial connection status to true or false
       setConnected(true);
       window.ethereum.on("accountsChanged", (accounts) => {
+        // update the connection status when the user changes accounts
         setAccount(accounts[0]);
         setConnected(true);
       });
       window.ethereum.on("disconnect", () => {
+        // update the connection status when the user disconnects
         setAccount("");
         setConnected(false);
-        setMinted(false);
+        setMinted(false); // Reset the minted state when the user disconnects
       });
       window.ethereum.enable().then((accounts) => {
         setAccount(accounts[0]);
-        const contractAddress = "0x32ff855a7cc4422d7fc2ee8b28c362b6e5282071";
-        const contractInstance = new web3.eth.Contract(
-          HogwartsNFT.abi,
-          contractAddress
-        );
-        setContract(contractInstance);
-        checkMinted();
+        const hogwartsAddress = "0x580462f5675dCDfb5f1806EfF4A19Cb668836E97";
+        const randomHouseAddress = "0xa8336934ca1934194Fdf098f19FEf6A8dd94FcBE";
+
+      const hogwartsInstance = new web3.eth.Contract(
+        HogwartsNFT.abi,
+        hogwartsAddress
+      );
+      const randomHouseInstance = new web3.eth.Contract(
+        RandomHouseAssignment.abi,
+        randomHouseAddress
+      );
+
+      setHogwartsContract(hogwartsInstance);
+      setRandomHouseContract(randomHouseInstance);
+    
+      checkMinted(); // Check for a minted NFT when the app first loads
+      getHouseData(); //Refresh the house data
       });
     } else {
       alert("Please install MetaMask to use this app!");
@@ -92,24 +63,11 @@ function App() {
   }, []);
 
   useEffect(() => {
-    if (contract || account) {
+    if (hogwartsContract|| randomHouseContract || account) {
+      getHouseData();
       checkMinted();
     }
-  }, [contract, account, checkMinted]);
-
-  useEffect(() => {
-    if (minted) {
-      getHouse();
-      getHouseSlogan();
-    }
-  }, [minted, getHouse, getHouseSlogan]);
-
-  // Update the useEffect to monitor changes in house and slogan data
-  useEffect(() => {
-    if (house !== "" && house_slogan !== "") {
-      setDataLoaded(true);
-    }
-  }, [house, house_slogan]);
+  }, [account]);
 
   const disconnectMetamask = async () => {
     try {
@@ -133,77 +91,107 @@ function App() {
   };
 
   const requestNFT = () => {
-    contract.methods
-      .requestNFT()
-      .send({ from: account, value: web3.utils.toWei("0", "ether") }) //if you add mint fees to the contract change this number accordingly
-      .on("transactionHash", function(hash) {
-        console.log("Transaction sent. Transaction hash:", hash);
-        setLoading(true); // Set loading to true before sending the transaction
+    randomHouseContract.methods
+        .requestNFT()
+        .send({ from: account, value: web3.utils.toWei("0", "ether") })
+        .on("transactionHash", function (hash) {
+            console.log("Transaction sent. Transaction hash:", hash);
+            setLoading(true); // Set loading to true before sending the transaction
 
-        // Play the thinking sound once the transaction is sent (user pays for the transaction)
-        thinkingAudio.play();
-      })
-      .on("receipt", function(receipt) {
-        console.log("Transaction successful:", receipt.transactionHash);
-        setMinted(true);
-        getHouse();
-        getHouseSlogan();
-       
-        // Play the house-specific audio after the transaction is confirmed
-        if (house !== "" && houseAudioMap.hasOwnProperty(house)) {
-          houseAudioMap[house].play();
-        }
-
-        if(dataLoaded){
-          setLoading(false); // Set loading back to false after the transaction is confirmed
-        }
-      })
-      .on("error", (error) => {
-        console.error("Error requesting NFT:", error);
-        setLoading(false); // Set loading back to false if there's an error during the transaction
-      });
-  };
+            // Play the thinking sound once the transaction is sent (user pays for the transaction)
+            thinkingAudio.play();
+        })
+        .on("receipt", function (receipt) {
+            console.log("Transaction successful:", receipt.transactionHash);
+            checkNewMinted();
   
- return (
-  <div className="App">
-    <img className="Hogwarts-logo" src={HogwartsLogo} alt="Hogwarts Logo" />
-    <h1>Welcome to Hogwarts</h1>
+        })
+        .on("error", (error) => {
+            console.error("Error requesting NFT:", error);
+            setLoading(false); // Set loading back to false if there's an error during the transaction
+        });
+};
+  
+  //function to get the house of the contract
+  const getHouseData = async () => {
+    setLoading(true);
+    const addressToHouse = ["You belong in Gryffindor....", "You belong in Hufflepuff....", "You belong in wise old Ravenclaw....", "You belong perhaps in Slytherin...."];
+    const houseIndex = await hogwartsContract.methods.getHouseIndex(account).call();
+    setHouse(addressToHouse[houseIndex]);
+    const sloganToHouse = [ "Where dwell the brave at heart. Their daring, nerve, and chivalry, Set Gryffindors apart.",    "Where they are just and loyal. Those patient Hufflepuffs are true And unafraid of toil.",    "If you’ve a ready mind. Where those of wit and learning, Will always find their kind.",    "You’ll make your real friends. Those cunning folks use any means, To achieve their ends."  ];      
+    sethouseSlogan(sloganToHouse[houseIndex]);
+    setLoading(false);
+  };
 
-    {connected ? (
-      <>
-        {minted ? (
-          <>
-            {loading ? (
-              // Display a loading message while the data is being loaded
-              <p>Ah, right then... hmm... right</p>
-            ) : dataLoaded ? (
-              // Display the house and slogan when the data is loaded
-              <>
-                <p>{house}</p>
-                {house_slogan.split('. ').map((slogan, index) => (
-                  <p key={index}>{slogan}</p>
-                ))}
-              </>
-            ) : (
-              // Fallback case when loading is complete but data hasn't loaded yet
-              <p>Hmm seems difficult</p>
-            )}
-          </>
-        ) : (
-          <button disabled={loading || minted} // Disable the button while loading or if already minted
-          onClick={requestNFT}>Let's choose your house </button>
-        )}
-        <button className="metamask-button" onClick={disconnectMetamask}>
-          Disconnect Wallet
+  // function to check if the user has minted an NFT
+  const checkMinted = async () => {
+    const minted = await hogwartsContract.methods.hasMintedNFT(account).call();
+    console.log(minted);
+    if (minted === true){
+      setMinted(true);
+      getHouseData();
+      setLoading(false);
+    }
+    else
+    setMinted(false);
+  };
+
+  // function to check if the user has minted an NFT
+  const checkNewMinted = async () => {
+      setTimeout(async() => {
+      const minted = await hogwartsContract.methods.hasMintedNFT(account).call();
+      console.log(minted, checkMintedSuccess);
+      if (minted === true){
+        setMinted(true);
+        getHouseData();
+        setLoading(false);
+      }
+      else if(checkMintedSuccess < 3){
+        setCheckMintSuccess(prev=>prev+1);
+        checkNewMinted();}
+      }, 800);
+    };
+  
+  return (
+    <div className="App">
+      <img className="Hogwarts-logo" src={HogwartsLogo} alt="Hogwarts Logo" />
+      <h1>Welcome to Hogwarts</h1>
+  
+      {connected ? (
+        <>
+          {minted ? (
+            <>
+              {loading ? (
+                // display a loading message while the data is being loaded
+                <p>Ah, right then... hmm... right</p>
+              ) : (
+                // display the house and slogan when the data is loaded
+                <>
+                  <p>{house}</p>
+                  {house_slogan.split('. ').map((slogan, index) => (
+                    <p key={index}>{slogan}</p>
+                  ))}
+                </>
+              )}
+            </>
+          ) : (
+            
+            <>
+              {!loading && <button onClick={requestNFT} disabled={minted}>Let's choose your house</button>}
+              {loading && <p className="loading-button-msg">Ah, right then... hmm... right</p>}
+            </>
+          )}
+          <button className="metamask-button" onClick={disconnectMetamask}>
+            disconnect wallet
+          </button>
+        </>
+      ) : (
+        <button className="metamask-button" onClick={connectMetamask}>
+          connect wallet
         </button>
-      </>
-    ) : (
-      <button className="metamask-button" onClick={connectMetamask}>
-        Connect Wallet
-      </button>
-    )}
-  </div>
-);
-}
-
-export default App;
+      )}
+    </div>
+  );
+}  
+    
+export default App;   
